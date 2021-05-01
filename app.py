a3dfdc95209a531800f5a3e653684d334b37e506
Rishi <rishiu@gmail.com>
2021-05-01 17:55:23 +0530

    m

--- app.py
+++ app.py
@@ -130,8 +130,8 @@ def hello():
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     # app.run()
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    # app.run(host='127.0.0.1', port=8080, debug=True)
 
